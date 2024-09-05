6188da4bed9860bd42ce5417c5b638f3ae4d559c
kate <kate@localhost.localdomain>
2024-09-05 09:40:36 +0300

    First commit

---
+++ Lesson1/Example/build/libmath/CMakeFiles/math_functions.dir/math_functions.cpp.o.d
@@ -0,0 +1,4 @@
+libmath/CMakeFiles/math_functions.dir/math_functions.cpp.o: \
+ /home/kate/Downloads/Hillel/Lesson1/Example/libmath/math_functions.cpp \
+ /usr/include/stdc-predef.h \
+ /home/kate/Downloads/Hillel/Lesson1/Example/libmath/math_functions.h
